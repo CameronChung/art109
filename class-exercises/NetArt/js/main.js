c8bd2f4334a5cb304bd0aca71f57d01c2d74cb5b
CameronChung <73194885+CameronChung@users.noreply.github.com>
2024-03-12 00:24:14 -0700

    netart project

---
+++ class-exercises/NetArt/js/main.js
@@ -0,0 +1,39 @@
+document.querySelector("#image-0").addEventListener("click", function(){
+    document.querySelector("#image-0").style.visibility = "hidden";
+    alert("...do you also find yourself lonely even when you're with other people?");
+})
+
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "hidden";
+    alert("...do you also feel like you can't relate to them?");
+})
+
+document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "hidden";
+    alert("...do you also avoid talking about yourself so you don't burden them?");
+})
+
+document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-3").style.visibility = "hidden";
+    alert("...do you feel like your voice is surpressed by the sound of theirs?");
+})
+
+document.querySelector("#image-4").addEventListener("click", function(){
+    document.querySelector("#image-4").style.visibility = "hidden";
+    alert("...do you start growing a hatred towards your own friends?");
+})
+
+document.querySelector("#image-5").addEventListener("click", function(){
+    document.querySelector("#image-5").style.visibility = "hidden";
+    alert("...are they actual friends?");
+})
+
+document.querySelector("#image-6").addEventListener("click", function(){
+    document.querySelector("#image-6").style.visibility = "hidden";
+    alert("...maybe I'm just overthinking.");
+})
+
+document.querySelector("#image-7").addEventListener("click", function(){
+    document.querySelector("#image-7").style.visibility = "hidden";
+    alert("...or is there something wrong we me?");
+})
