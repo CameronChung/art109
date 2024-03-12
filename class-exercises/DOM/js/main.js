25fabdf9f8ab2ae198ab00467114762b052e57f5
CameronChung <73194885+CameronChung@users.noreply.github.com>
2024-03-11 21:54:04 -0700

    DOM excercise

---
+++ class-exercises/DOM/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector("#image-0").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "visible";
+
+})
+
+document.querySelector("#image-1").addEventListener("click", function(){
+})
+
