4b846455bfb7051c8cf1c5130a3d8b002eab5421
CameronChung <73194885+CameronChung@users.noreply.github.com>
2024-05-07 16:32:26 -0700

    adding css

---
+++ class-exercises/finalProject/js/main.js
@@ -0,0 +1,7 @@
+//~~~~~~~Import Three.js (also linked to as an import map in the HTML)~~~~~~
+import * as THREE from 'three';
+
+
+// Import add-ons
+import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js';
