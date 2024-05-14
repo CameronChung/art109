eab877a3ebc18c2dc7177f0541d96bcee7910b43
CameronChung <73194885+CameronChung@users.noreply.github.com>
2024-05-13 22:47:14 -0700

    adding final project files

--- class-exercises/finalProject/js/main.js
+++ class-exercises/finalProject/js/main.js
@@ -5,3 +5,92 @@ import * as THREE from 'three';
 // Import add-ons
 import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js';
+
+
+// ~~~~~~~~~~~~~~~~ Declare Global Variables~~~~~~~~~~~~~~~~
+let scene, camera, renderer, chair, beach, mixer;
+
+
+// ~~~~~~~~~~~~~~~~ Initialize Scene in init() ~~~~~~~~~~~~~~~~
+function init() {
+
+    // ~~~~~~Set up scene, camera, + renderer ~~~~~~
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x058899);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+
+    // ~~~~~~ Add Lights ~~~~~~
+    // Add helpers to debug the lights' position - COMMENT OUT WHEN DONE placing the light! https://threejs.org/docs/#api/en/helpers/DirectionalLightHelper
+
+    // ~~ add directional light 
+    const lightRight = new THREE.DirectionalLight(0xffffff, 3);
+    lightRight.position.set(3, 4, 5);
+    scene.add(lightRight);
+
+    const helperRight = new THREE.DirectionalLightHelper(lightRight, 5);
+    // scene.add(helperRight); // comment out when done placing light
+
+
+    // ~~ add directional light 
+    const lightLeft = new THREE.DirectionalLight(0xffff000, 3);
+    lightLeft.position.set(-3, 4, 5);
+    scene.add(lightLeft);
+
+    const helperLeft = new THREE.DirectionalLightHelper(lightLeft, 5);
+    // scene.add(helperLeft); // comment out when done placing light
+
+
+
+    // ~~~~~~ Initiate add-ons ~~~~~~
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const loader = new GLTFLoader(); // to load 3d models
+
+    // --> Load glTF
+
+    // load models
+
+    loader.load('assets/chair/scene.gltf', function (gltf) {
+        chair = gltf.scene;
+        scene.add(chair);
+        chair.scale.set(1, 1, 1); // scale your model
+        chair.position.y = -2; // set initial position
+
+    }, undefined, function(error) {
+        console.error(error);
+    
+    });
+
+    // loader.load('assets/at_a_beach/scene.gltf', function (gltf){
+    //     beach = gltf.scene;
+    //     scene.add(beach);
+    //     beach.scale.set(1.2, 1.2, 1.2); // scale your model
+    //     beach.position.y = -2; // set initial position
+    //     beach.position.z = -4;
+    // });
+
+
+
+    // ~~~~~~Position Camera~~~~~~
+    camera.position.z = 5;
+
+
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init(); // execute initialize function
+// animate(); // execute animation function
